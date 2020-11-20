dae7923f2efca044c30ec1d022e3e1743b579bd3
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-20 17:29:38 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan58-TambahKurang/src/if1/pkg10117126/latihan58/tambahkurang/IF110117126Latihan58TambahKurang.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan58.tambahkurang;
+
+/**
+ *Nama  : Mawa Faqu Rochman
+ *Kelas : IF-1
+ *NIM   : 10117126
+ *Deskripsi Program : program ini berisi program hasil penjumlahan dan selisih
+ */
+
+public class IF110117126Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumbil = new JumlahBilangan();
+        jumbil.tampilHasilJumlah();
+
+        SelisihBilangan selisih = new SelisihBilangan();
+        selisih.tampilSelisih();
+    }
+
+}
